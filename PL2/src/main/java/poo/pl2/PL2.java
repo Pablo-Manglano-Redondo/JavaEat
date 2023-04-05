95263c430547b269f02496fc5f7148b067b3e6a1
pablo <pablo.manglano@edu.uah.es>
2023-04-05 20:15:11 +0200

    Se añaden documentos varios y proyecto inicio

---
+++ PL2/src/main/java/poo/pl2/PL2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pl2;
+
+/**
+ *
+ * @author pxblx
+ */
+public class PL2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
